c19930539fa97f1687c4386bb112d4ea69f98b76
adisunjana <adisunjana443@gmail.com>
2026-01-01 18:13:18 +0700

    fix error bluetooth

--- lib/providers/printer_provider.dart
+++ lib/providers/printer_provider.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:print_bluetooth_thermal/print_bluetooth_thermal.dart';
 import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:io'; // Untuk cek Platform
 
 class PrinterProvider with ChangeNotifier {
   // Menggunakan library print_bluetooth_thermal
@@ -16,14 +18,58 @@ class PrinterProvider with ChangeNotifier {
   String? get connectedMacAddress => _connectedMacAddress;
 
   // 1. Scan Perangkat Bluetooth
+  // Future<void> scanDevices() async {
+  //   _isLoading = true;
+  //   notifyListeners();
+
+  //   try {
+  //     final List<BluetoothInfo> result =
+  //         await PrintBluetoothThermal.pairedBluetooths;
+  //     _devices = result;
+  //   } catch (e) {
+  //     debugPrint("Error scan devices: $e");
+  //   } finally {
+  //     _isLoading = false;
+  //     notifyListeners();
+  //   }
+  // }
+
   Future<void> scanDevices() async {
     _isLoading = true;
     notifyListeners();
 
     try {
-      final List<BluetoothInfo> result =
-          await PrintBluetoothThermal.pairedBluetooths;
-      _devices = result;
+      bool permissionGranted = false;
+
+      if (Platform.isAndroid) {
+        // Cek versi Android atau minta permission secara umum
+        // Permission.bluetoothConnect wajib untuk Android 12+ agar bisa baca paired devices
+        // Permission.bluetoothScan wajib untuk scan device baru
+        // Permission.location wajib untuk Android 11 ke bawah
+
+        Map<Permission, PermissionStatus> statuses = await [
+          Permission.bluetooth,
+          Permission.bluetoothScan,
+          Permission.bluetoothConnect,
+          Permission.location,
+        ].request();
+
+        if (statuses[Permission.bluetoothConnect] == PermissionStatus.granted ||
+            statuses[Permission.bluetooth] == PermissionStatus.granted) {
+          permissionGranted = true;
+        }
+      } else {
+        // iOS biasanya otomatis handle via Info.plist, anggap granted
+        permissionGranted = true;
+      }
+
+      if (permissionGranted) {
+        final List<BluetoothInfo> result =
+            await PrintBluetoothThermal.pairedBluetooths;
+        _devices = result;
+      } else {
+        debugPrint("Izin Bluetooth ditolak");
+      }
     } catch (e) {
       debugPrint("Error scan devices: $e");
     } finally {
